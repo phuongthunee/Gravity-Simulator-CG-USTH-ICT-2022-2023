e53c7fc65a94be60000c0e10318e7f4f56b39d48
phuongthunee <lephuongthu18@gmail.com>
2023-03-30 01:25:14 +0700

    create sphere and atmosphere, add image and animation

---
+++ gravity-simulator/main.js
@@ -0,0 +1,66 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer({antialias: true})
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+
+//create a sphere
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
+new THREE.ShaderMaterial({
+  vertexShader: vertexShader, 
+  fragmentShader: fragmentShader,
+  uniforms: { globeTexture: { value: new THREE.TextureLoader().load('./images/EarthUVMap.jpg')}}
+}))
+
+scene.add(sphere)
+
+//Create atmosphere
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide
+  })
+)
+atmosphere.scale.set(1.1, 1.1, 1.1)
+scene.add(atmosphere)
+
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group)
+
+camera.position.z = 15
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+  sphere.rotation.y += 0.001
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.5,
+    y: mouse.x *0.5, 
+    duration: 2})
+}
+animate()
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1
+  console.log(mouse)
+})
